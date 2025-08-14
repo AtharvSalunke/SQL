790aa639299eda29930a396193ffd01984b07788
Aharv123 <atharvsalunke@gmail.com>
2025-08-14 14:32:52 +0530

    codes

--- students.sql
+++ students.sql
@@ -1,1 +1,17 @@
-Select * from student
+create database temp;
+
+use temp;
+
+create table student (
+id int primary key,
+name varchar(255)
+);
+
+insert into student (id,name) 
+values 
+(1,'Atharv'),
+(2,'Raj'),
+(3,'Parjeet');
+
+
+select * from student;
